d221eddc5333dacdbf4b7f3ede2e586d3771c3c7
helkprogram <h.eeelk@gmail.com>
2020-05-17 22:54:09 +0300

    without responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(function () {
-
+    // $("#menu").on("click", "a", function (event) {
+    //     event.preventDefault();
+    //     var id = $(this).attr('href'),
+    //         top = $(id).offset().top;
+    //     $('body,html').animate({ scrollTop: top }, 1500);
+    // });
+    $('.menu__btn').on('click', function (e) {
+        e.preventDefault;
+        $(this).toggleClass('menu__btn--active');
+        $('.menu__list').slideToggle();
+    });
+    $('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header-dots',
+        autoplay: 2000,
+        pauseOnHover: true,
+        verticalSwiping: true
+    });
 });
